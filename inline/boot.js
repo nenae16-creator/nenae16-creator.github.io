a26c637790d89daf854a93e408dfea4e95d5b622
nenae16-creator <nenae16@gmail.com>
2026-09-05 16:50:17 +0900

    Load compact app in two parts

--- inline/boot.js
+++ inline/boot.js
@@ -6,7 +6,7 @@
     if (view) view.innerHTML = '<div class="card"><h2>' + msg + '</h2><p class="muted">잠시만 기다려 주세요.</p></div>';
   }
   async function grab(url) {
-    var res = await fetch(url + (url.indexOf("?") >= 0 ? "&" : "?") + "v=20260905c");
+    var res = await fetch(url + (url.indexOf("?") >= 0 ? "&" : "?") + "v=20260905d");
     if (!res.ok) throw new Error(url + " " + res.status);
     return res.text();
   }
@@ -21,9 +21,9 @@
     return;
   }
   try {
-    var parts = [];
-    for (var i = 1; i <= 5; i++) parts.push(await grab("app.p" + i + ".js"));
-    eval(parts.join(""));
+    var a = await grab("app.p1.js");
+    var b = await grab("app.p2.js");
+    eval(a + b);
   } catch (e) {
     show("앱 코드를 불러오지 못했습니다");
     console && console.error && console.error(e);
